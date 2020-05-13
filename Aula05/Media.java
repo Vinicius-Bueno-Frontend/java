d1d3b159c383745aa47f2068166ae8cf14082f3b
ViniciusGenthix <60518219+ViniciusGenthix@users.noreply.github.com>
2020-05-13 15:27:38 -0300

    Inserção do "Author"

--- Aula05/Media.java
+++ Aula05/Media.java
@@ -3,17 +3,23 @@ package exercicios;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Vinicius Bueno Silva
+ *
+ */
+
 public class Media {
 
 	public static void main(String[] args) {
-		//<ctrl><shift><f> (alinhamento do código)//
+		//<ctrl><shift><f> (alinhamento do cÃ³digo)//
 		//variaveis
 		double nota1, nota2, media;
 		//objeto leitor (usado para capturar dados)
 		Scanner leitor = new Scanner(System.in);
 		//objeto formatador (usado para formatar o resultado)
 		DecimalFormat formatador = new DecimalFormat("0.0");
-		System.out.println("Cálculo da média");
+		System.out.println("CÃ¡lculo da mÃ©dia");
 		//entrada
 		System.out.print("Digite a nota1: ");
 		nota1 = leitor.nextDouble();
@@ -22,11 +28,11 @@ public class Media {
 		//processamento
 		media = (nota1 + nota2) / 2;
 		//saida
-		System.out.println("Média: " + formatador.format(media));
+		System.out.println("MÃ©dia: " + formatador.format(media));
 		if (media < 2) {
 			System.out.println("Reprovado");
 		} else if (media >= 2 && media < 5) {
-			System.out.println("Recuperação");
+			System.out.println("RecuperaÃ§Ã£o");
 		} else {
 			System.out.println("Aprovado");
 		}
