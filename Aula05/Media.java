18386e6de85aa981a5d69a50d9feee20a2efdb1f
ViniciusGenthix <60518219+ViniciusGenthix@users.noreply.github.com>
2020-05-13 14:41:34 -0300

    Média

---
+++ Aula05/Media.java
@@ -0,0 +1,38 @@
+package exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Media {
+
+	public static void main(String[] args) {
+		//<ctrl><shift><f> (alinhamento do código)//
+		//variaveis
+		double nota1, nota2, media;
+		//objeto leitor (usado para capturar dados)
+		Scanner leitor = new Scanner(System.in);
+		//objeto formatador (usado para formatar o resultado)
+		DecimalFormat formatador = new DecimalFormat("0.0");
+		System.out.println("Cálculo da média");
+		//entrada
+		System.out.print("Digite a nota1: ");
+		nota1 = leitor.nextDouble();
+		System.out.print("Digite a nota2: ");
+		nota2 = leitor.nextDouble();
+		//processamento
+		media = (nota1 + nota2) / 2;
+		//saida
+		System.out.println("Média: " + formatador.format(media));
+		if (media < 2) {
+			System.out.println("Reprovado");
+		} else if (media >= 2 && media < 5) {
+			System.out.println("Recuperação");
+		} else {
+			System.out.println("Aprovado");
+		}
+		
+		leitor.close();
+		
+	}
+	
+}
